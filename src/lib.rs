529221c35c5c1e5e2e85435ba98ae2e6cf492bb2
crhino <piraino.chris@gmail.com>
2015-09-07 00:00:40 -0700

    Add more documentation on lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 //!
 //! Currently supporting only UDP, the idea is to create easy abstraction layers around transport
 //! protocols. Do not think about sending byte arrays around or handing sockets/file descriptors.
+//!
+//! For serialization, Transit depends on the `serde` framework. Both JSON encoding and Msgpack
+//! encoding are supported through the use of features. Make sure to compile transit with either
+//! the `json_serialization` or the `msgpack_serialization` feature.
 #![cfg_attr(test, feature(custom_derive, plugin))]
 #![cfg_attr(test, plugin(serde_macros))]
 
